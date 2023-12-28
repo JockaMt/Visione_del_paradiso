4224e44ec973e14b6c5637b9d7bc49dd6a4f2de0
jockaplay <jocksonmatheus@gmail.com>
2023-12-28 08:45:22 -0300

    ⚙️ - Testing routes

--- api_vdp/src/views.py
+++ api_vdp/src/views.py
@@ -3,4 +3,8 @@ from flask import render_template, url_for
 def init_app(app):
     @app.route("/")
     def home():
-        return render_template("home/index.html", name="Hello World")
+        return render_template("home.html", name="Hello World")
+    
+    @app.route("/login")
+    def login():
+        return render_template("login.html")
