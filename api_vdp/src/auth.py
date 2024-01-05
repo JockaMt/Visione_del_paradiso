371c6297e3cbdf57203cb15d99938ff627e13836
JairontSantos <jairon.tavares@hotmail.com>
2024-01-05 14:10:31 -0300

    Update auth.py
    teste

--- api_vdp/src/auth.py
+++ api_vdp/src/auth.py
@@ -2,17 +2,17 @@ def cripto(senha):
     cod = ""
 
     for i in senha:
-        cod = cod + chr(ord(i) + 7)
+        cod = cod + chr((ord(i) + 7) % 127)
     return cod
 
-def descripto (cod):
+def descripto(cod):
     senha = ""
 
     for i in cod:
-        senha = senha + chr(ord(i) - 7)
+        senha = senha + chr((ord(i) - 7) % 127)
     return senha
 
-palavra = cripto("eu tenho depressao")
+palavra = cripto("Estou criando um projeto sobre gerenciamento de hotéis, e preciso de alguma forma testar essa senha e esse tipo de criptografia, então, acho que é isso")
 
 print(palavra)
 
