81a15ded5fa1a46da5e121e446722703babcc852
jockaplay <jocksonmatheus@gmail.com>
2024-02-03 17:40:30 -0300

    Add function to another pages.

--- api_vdp/src/views.py
+++ api_vdp/src/views.py
@@ -16,7 +16,22 @@ def init_app(app):
     @app.route("/profile", methods=['GET', 'POST'])
     def profile():
         return profile_page()
+
+    #TODO: escrever pagina de editar perfil.
+    @app.route("/edit-profile", methods=['GET', 'POST'])
+    def edit_profile():
+        return home_page()
+
+    #TODO: Escrever pagina de quartos.
+    @app.route("/rooms", methods=['GET', 'POST'])
+    def rooms():
+        return home_page()
     
     @app.route("/logout")
     def logout():
         return logout_page()
+
+    # TODO: Escrever pagina de checkin.
+    @app.route("/checkin/<id>", methods=['GET', 'POST'])
+    def checkin(id):
+        return home_page()
